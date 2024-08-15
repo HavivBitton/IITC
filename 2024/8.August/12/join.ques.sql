bd8f9415191a75bef29c315b2292ede5132f5d7c
HavivBitton <havivbiton4@gmail.com>
2024-08-15 12:18:49 +0300

    join done

--- 2024/8.August/12/join.ques.sql
+++ 2024/8.August/12/join.ques.sql
@@ -82,3 +82,67 @@
 -- ////////////////////////////////////////////////////////////////////////////////////
 
 -- Q9
+-- SELECT 
+-- pro.ProductName,
+-- cat.[Description],
+-- sup.City
+-- from Products pro join Categories cat
+-- on pro.CategoryID = cat.CategoryID
+-- join Suppliers sup on pro.SupplierID = sup.SupplierID
+-- WHERE sup.City ='london' or sup.City = 'tokyo'
+
+-- ////////////////////////////////////////////////////////////////////////////////////
+
+-- Q10
+-- SELECT
+-- pro.ProductID,
+-- cat.[Description],
+-- sup.Country
+-- from Products pro join Categories cat
+-- on pro.CategoryID = cat.CategoryID
+-- join Suppliers sup on pro.SupplierID = sup.SupplierID
+-- where sup.Country like 'a%'
+
+
+-- ////////////////////////////////////////////////////////////////////////////////////
+
+-- Q11
+-- SELECT 
+-- CompanyName,
+-- OrderID
+-- from Customers c left JOIN Orders o
+-- on c.CustomerID = o.CustomerID
+
+-- ////////////////////////////////////////////////////////////////////////////////////
+
+-- Q12
+-- SELECT
+-- o.OrderID 'order id',
+-- o.OrderDate 'order date',
+-- o.ShipAddress 'ship address',
+-- c.CustomerID 'customers id',
+-- c.CompanyName 'company name',
+-- c.Phone 'phone number'
+-- from Orders o join Customers c
+-- on o.CustomerID = c.CustomerID
+-- WHERE YEAR(OrderDate) = 1996
+-- AND (c.CustomerID like 'a%' or c.CustomerID like 'c%')
+
+-- ////////////////////////////////////////////////////////////////////////////////////
+
+-- Q12
+-- SELECT
+-- e.FirstName + ' ' + e.LastName 'Name',
+-- o.OrderID 'order id',
+-- o.OrderDate 'order date',
+-- o.ShipAddress 'ship address',
+-- c.CustomerID 'customers id',
+-- c.CompanyName 'company name',
+-- c.Phone 'phone number'
+-- from Orders o join Customers c
+-- on o.CustomerID = c.CustomerID
+-- JOIN Employees e on e.EmployeeID = o.EmployeeID
+-- WHERE YEAR(OrderDate) = 1996
+-- AND (c.CustomerID like 'a%' or c.CustomerID like 'c%')
+-- ORDER by o.OrderDate DESC
+ 
