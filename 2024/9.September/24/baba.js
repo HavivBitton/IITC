fdac7a49f0ab398174e0f24ff0a1df6691f0f9dd
HavivBitton <havivbiton4@gmail.com>
2024-09-24 15:45:50 +0300

    24.9

---
+++ 2024/9.September/24/baba.js
@@ -0,0 +1,9 @@
+import { friends } from "./friends.js";
+
+const baba = {
+  name: "baba",
+  age: 14,
+  friendsList: friends,
+};
+
+export default baba;
---
+++ 2024/9.September/24/main.js
@@ -0,0 +1,8 @@
+import baba from "./baba.js";
+
+console.log(
+  `Hello from ${baba.name}
+    im  ${baba.age} years old,
+    my friends are ${baba.friendsList} 
+    `
+);
