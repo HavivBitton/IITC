938184ef7cc8bc00674da43c77ec5f49867a0361
HavivBitton <havivbiton4@gmail.com>
2024-09-29 17:41:39 +0300

    29.9

--- 2024/9.September/29/inClass/main.js
+++ 2024/9.September/29/inClass/main.js
@@ -50,3 +50,46 @@ function displayFilterArray(peopleArray) {
 }
 
 displayFilterArray(people);
+
+//Q3
+
+const fruits = ["apple", "banana"];
+
+function displayFruitsArray(fruits) {
+  const fruitsObj = fruits.reduce((acc, string) => {
+    const stringArr = string.split("");
+    stringArr.forEach((char) => {
+      if (acc[char]) acc[char]++;
+      else acc[char] = 1;
+    });
+    return acc;
+  }, {});
+  console.log(fruitsObj);
+  //{a: 4, p: 2, l: 1, e: 1, b: 1, n:2}
+
+  // Create a DOM element in the html and reset him.
+  const fruitsList = document.getElementById("fruitsList");
+  fruitsList.innerHTML = "";
+}
+
+displayFruitsArray(fruits);
+
+const products = [
+  { name: "Laptop", price: 4000, quantity: 10 },
+  { name: "Smartphone", price: 2000, quantity: 5 },
+  { name: "Headphones", price: 150, quantity: 0 }, // This product is out of stock
+  { name: "Monitor", price: 800, quantity: 7 },
+  { name: "Keyboard", price: 100, quantity: 3 },
+  { name: "Smartwatch", price: 1800, quantity: 0 }, // Another product out of stock
+];
+
+const findProducts = products.find((product) => {
+  return product.quantity === 0;
+});
+console.log(findProducts);
+
+const findProductsIndex = products.findIndex((product) => {
+  return product.quantity === 0;
+});
+console.log(findProductsIndex);
+console.log(products[findProductsIndex]);
---
+++ Employee List project/static-data.js
@@ -0,0 +1,44 @@
+const data = [
+  {
+    firstName: "Sarah",
+    lastName: "Levi",
+    age: 30,
+    startDate: "02/15/2020",
+    department: "Marketing",
+    salary: 95000,
+  },
+  {
+    firstName: "Daniel",
+    lastName: "Cohen",
+    age: 28,
+    startDate: "05/10/2021",
+    department: "QA",
+    salary: 85000,
+  },
+  {
+    firstName: "Maya",
+    lastName: "Rosen",
+    age: 26,
+    startDate: "08/01/2022",
+    department: "Develop",
+    salary: 100000,
+  },
+  {
+    firstName: "David",
+    lastName: "Katz",
+    age: 32,
+    startDate: "03/25/2019",
+    department: "Marketing",
+    salary: 105000,
+  },
+  {
+    firstName: "Noa",
+    lastName: "Bar",
+    age: 27,
+    startDate: "11/12/2021",
+    department: "Develop",
+    salary: 98000,
+  },
+];
+
+export const randomData = { data };
