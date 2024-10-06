b29d94ecba9a8beceddbad742ba60a5c3387b9b7
HavivBitton <havivbiton4@gmail.com>
2024-10-06 09:58:58 +0300

    6.10

---
+++ 2024/10.Octeber/1/main.js
@@ -0,0 +1,17 @@
+// Q1
+
+const number = [5, 4, 3, 2, 1];
+const consoleArr = number.forEach((num) => {
+  console.log(num);
+});
+
+//Q2
+
+const doubleNum = number.forEach((num) => {
+  console.log(num * 2);
+});
+
+//Q3
+const doubleNum1 = number.forEach((num) => {
+  console.log(num * 2);
+});
