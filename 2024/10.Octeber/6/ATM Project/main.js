59c020903663cabb7815e1dd51cf49fe27b46ab0
HavivBitton <havivbiton4@gmail.com>
2024-10-07 22:59:42 +0300

    break

--- 2024/10.Octeber/6/ATM Project/main.js
+++ 2024/10.Octeber/6/ATM Project/main.js
@@ -4,18 +4,22 @@
 
 // let gCostumerList = utils.getFromStorage(COSTUMER_STOREGE_KEY);
 
+// Define code
 const demoCode = 1234;
 
+// Get elements from the DOM
 const elCodeForm = document.querySelector("#codeForm");
 const elCodeMessage = document.querySelector("#codeMessage");
 const elNavMenu = document.querySelector("#nav-menu");
 
+// Add event listener to the form
 elCodeForm.addEventListener("submit", function (ev) {
   ev.preventDefault();
   elCodeMessage.classList.remove("correct", "incorrect");
 
   const elInputCode = document.querySelector("#pinCode");
 
+  //Validates the entered code and displays an appropriate message
   if (elInputCode.value == demoCode) {
     elCodeMessage.textContent = "correct code";
     elCodeMessage.classList.add("correct");
@@ -23,5 +27,30 @@ elCodeForm.addEventListener("submit", function (ev) {
   } else {
     elCodeMessage.textContent = "incorrect code";
     elCodeMessage.classList.add("incorrect");
+    elNavMenu.classList.add("hide");
   }
 });
+
+// Shoe balance button
+//
+// Define balance
+const demoBalance = 10000;
+
+// Get elements from the DOM
+const elBalance = document.querySelector("#balance");
+const elBalanceBtn = document.querySelector("#balanceButton");
+
+// Add event listener to the button
+elBalanceBtn.addEventListener("click", function () {
+  ShowBalance();
+});
+
+// Function to display balance
+function ShowBalance() {
+  elBalance.textContent = `Your balance is ${demoBalance}`;
+  elBalance.classList.remove("hide");
+}
+
+//
+// Get elements from the DOM
+const elDepositBtn = document.querySelector("#DepositButton");
