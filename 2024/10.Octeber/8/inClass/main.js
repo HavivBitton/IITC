f19a91c6b2e208234bcfa1721d210612f565e5cc
HavivBitton <havivbiton4@gmail.com>
2024-10-08 16:15:47 +0300

    in class end

---
+++ 2024/10.Octeber/8/inClass/main.js
@@ -0,0 +1,23 @@
+const imgDiv = document.querySelector("#api1");
+const textDiv = document.querySelector("#api2");
+
+function insertImage(src) {
+  imgDiv.insertAdjacentHTML("afterbegin", `<img src="${src}" alt="Dog Image">`);
+}
+function insertText(src) {
+  textDiv.insertAdjacentHTML("afterbegin", `<h1>${src}</h1>`);
+}
+
+fetch("https://dog.ceo/api/breeds/image/random")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    insertImage(data.message);
+  });
+
+fetch("https://cat-fact.herokuapp.com/facts")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    insertText(data[1].text);
+  });
