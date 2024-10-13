3e88ef0e0a68c1b561d37ac5b5b37a8675bca412
HavivBitton <havivbiton4@gmail.com>
2024-10-13 18:26:11 +0300

    break

---
+++ Fronted mentor/faq-accordion-main/main.js
@@ -0,0 +1,31 @@
+// Get element from thee DOM
+const elSubmitBtn = document.querySelector("#submitBtn");
+const elRatePageDiv = document.querySelector("#ratePage");
+const elTnxPageDiv = document.querySelector("#tnxPage");
+const elNumButton = document.querySelectorAll(".circle");
+const elRateMsgDiv = document.querySelector("#rateMsg");
+let rateNum = 0;
+
+elSubmitBtn.addEventListener("click", function () {
+  const active = document.querySelector(".selected");
+  if (active) {
+    // Show the tnx page
+    elRatePageDiv.classList.add("hide");
+    //Hide the rate page
+    elTnxPageDiv.classList.remove("hide");
+    // Add the rate number to the message
+    elRateMsgDiv.innerHTML = `You selected ${rateNum} out of 5`;
+  }
+});
+
+elNumButton.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    const active = document.querySelector(".selected");
+    if (active) {
+      active.classList.remove("selected");
+    }
+    button.classList.add("selected");
+
+    rateNum = e.target.innerText;
+  });
+});
