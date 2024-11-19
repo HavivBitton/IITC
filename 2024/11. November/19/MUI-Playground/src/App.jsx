51c89ccd9db5dfd5a2a2b02a32bc2da71f763e9d
HavivBitton <havivbiton4@gmail.com>
2024-11-19 12:00:36 +0200

    break

--- 2024/11. November/19/MUI-Playground/src/App.jsx
+++ 2024/11. November/19/MUI-Playground/src/App.jsx
@@ -2,7 +2,25 @@ import { useState } from "react";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-import TextField from "@mui/material/TextField";
+import { TextField, Avatar } from "@mui/material";
+
+const Dummy_Users = [
+  {
+    id: 1,
+    fullName: "Yossef Sabag",
+    url: "",
+  },
+  {
+    id: 2,
+    fullName: "Eden Shabi",
+    url: "https://friendsmerchandise.com/wp-content/uploads/2021/09/friends-smelly-cat-enamel-pin-1.png",
+  },
+];
+
+const getInitial = (fullName) => {
+  const nameArr = fullName.split(" ");
+  return nameArr[0][0] + nameArr[1][0];
+};
 
 function App() {
   return (
@@ -11,14 +29,28 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        <div>
+          {Dummy_Users.map((user) => {
+            getInitial(user.fullName);
+            return (
+              <div key={user.id}>
+                <Avatar src={user.url} sx={{ width: "120px", height: "120px" }}>
+                  {getInitial(user.fullName)}
+                </Avatar>
+                .<p>{user.fullName}</p>
+              </div>
+            );
+          })}
+        </div>
+
+        {/* <TextField id="outlined-basic" label="Outlined" variant="outlined" />
         <TextField id="filled-basic" label="Filled" variant="filled" />
         <TextField
           sx={{ input: { color: "#4287f5" } }}
           id="standard-basic"
           label="Standard"
           variant="standard"
-        />
+        /> */}
       </div>
     </>
   );
