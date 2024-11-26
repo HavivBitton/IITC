e02dde5711c9028c521a00236edb5dcd9d385f3d
HavivBitton <havivbiton4@gmail.com>
2024-11-26 13:42:51 +0200

    to do list done

---
+++ 2024/11. November/26/Redux-Learning/src/store/reducers/TodoReducer.js
@@ -0,0 +1,37 @@
+const initialState = {
+  todos: [
+    { id: 1111, text: "first todo", completed: false },
+    { id: 1112, text: "second todo", completed: false },
+  ],
+};
+
+const todosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          { id: Date.now(), text: action.payload, completed: false },
+        ],
+      };
+    case "DELETE_TODO":
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    case "TOGGLE_TODO":
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload
+            ? { ...todo, completed: !todo.completed }
+            : todo
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export default todosReducer;
