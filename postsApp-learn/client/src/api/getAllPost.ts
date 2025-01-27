3c05ca63684b48601de9a6ac1c8e2b377b915975
HavivBitton <havivbiton4@gmail.com>
2025-01-27 10:21:42 +0200

    /

--- postsApp-learn/client/src/api/getAllPost.ts
+++ postsApp-learn/client/src/api/getAllPost.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
-import { Post } from "../types.ts";
 
 const getAllPosts = async () => {
   try {
-    const response = await axios.get<Post[]>(
-      `http://localhost:3000/api/posts`,
-      { withCredentials: true }
-    );
+    const response = await axios.get(`http://localhost:3000/api/posts`, {
+      withCredentials: true,
+    });
     console.log(response);
     return response.data;
   } catch (error) {
